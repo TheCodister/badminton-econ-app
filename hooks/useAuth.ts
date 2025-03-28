3951424496a54145b662d651eb5b80d62893a3bb
TheCodister <quangcuber002@gmail.com>
2025-03-28 21:59:16 +0700

    fix: delete useAuth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,42 +1,42 @@
-import Cookies from 'js-cookie'
-import { useEffect, useState } from 'react'
+// import Cookies from 'js-cookie'
+// import { useEffect, useState } from 'react'
 
-export const useAuth = () => {
-  const [user, setUser] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+// export const useAuth = () => {
+//   const [user, setUser] = useState(null)
+//   const [loading, setLoading] = useState(true)
+//   const [error, setError] = useState(null)
 
-  useEffect(() => {
-    const token = Cookies.get('access_token')
+//   useEffect(() => {
+//     const token = Cookies.get('access_token')
 
-    if (token) {
-      // Make request to get the current logged-in user
-      const username = localStorage.getItem('username') // Assuming you save username in localStorage during login
+//     if (token) {
+//       // Make request to get the current logged-in user
+//       const username = localStorage.getItem('username') // Assuming you save username in localStorage during login
 
-      fetch(`/users/${username}`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error('Failed to authenticate user')
-          }
-          return res.json()
-        })
-        .then((data) => {
-          setUser(data) // Store user data
-          setLoading(false)
-        })
-        .catch((err) => {
-          setError(err.message)
-          setLoading(false)
-        })
-    } else {
-      setLoading(false)
-    }
-  }, [])
+//       fetch(`/users/${username}`, {
+//         method: 'GET',
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       })
+//         .then((res) => {
+//           if (!res.ok) {
+//             throw new Error('Failed to authenticate user')
+//           }
+//           return res.json()
+//         })
+//         .then((data) => {
+//           setUser(data) // Store user data
+//           setLoading(false)
+//         })
+//         .catch((err) => {
+//           setError(err.message)
+//           setLoading(false)
+//         })
+//     } else {
+//       setLoading(false)
+//     }
+//   }, [])
 
-  return { user, loading, error }
-}
+//   return { user, loading, error }
+// }
