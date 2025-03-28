974cfe532ffbbb316bd029eec369e932e4b18015
TheCodister <quangcuber002@gmail.com>
2025-03-28 22:10:38 +0700

    fix: change backend url

--- constants/base_url.ts
+++ constants/base_url.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = 'http://localhost:3000'
 export const BACKEND_URL =
   process.env.ENV === 'Development'
-    ? 'https://badminton-econ-app.onrender.com'
-    : 'http://localhost:3001'
+    ? 'http://localhost:3001'
+    : 'https://badminton-econ-app.onrender.com'
