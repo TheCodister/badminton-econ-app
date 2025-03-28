d8e9cd5dd14a8c2ca6ea8a5387a57b5b21484951
TheCodister <quangcuber002@gmail.com>
2025-03-28 22:18:14 +0700

    fix: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,13 @@
 import { NestFactory } from '@nestjs/core'
-import * as cors from 'cors'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  // Use cors middleware
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'https://badminton-econ-app.vercel.app/',
-      ], // Allowed origins
-      credentials: true,
-    }),
-  )
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://badminton-econ-app.vercel.app'], // Allowed origins
+    credentials: true,
+  })
 
   await app.listen(3001)
 }
