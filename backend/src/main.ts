0b913d2e9a75a2d65d1133edf052080746b4c99b
TheCodister <quangcuber002@gmail.com>
2025-03-28 22:38:01 +0700

    fix: add 1 more cors origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://badminton-econ-app.vercel.app'], // Allowed origins
+    origin: [
+      'http://localhost:3000',
+      'https://badminton-econ-app.vercel.app',
+      'https://badminton-econ-app-nkv4.vercel.app',
+    ], // Allowed origins
     credentials: true,
   })
 
