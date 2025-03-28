77346f83470b3825467ab759274e095aebeae323
TheCodister <quangcuber002@gmail.com>
2025-03-28 21:26:23 +0700

    fix: vercel deployment error

--- constants/base_url.ts
+++ constants/base_url.ts
@@ -1,2 +1,5 @@
 export const BASE_URL = 'http://localhost:3000'
-export const BACKEND_URL = 'http://localhost:3001'
+export const BACKEND_URL =
+  process.env.ENV === 'Development'
+    ? 'http://localhost:3001'
+    : 'https://badminton-econ-app.onrender.com'
