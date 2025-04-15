829bb573a375c8af96c8776a1f8fb0ba69696587
NMichaelg <107455689+NMichaelg@users.noreply.github.com>
2025-04-15 11:12:02 +0000

    bugged found, some product got table info in description instead

--- scraper/main.py
+++ scraper/main.py
@@ -13,6 +13,7 @@ import datetime
 import argparse
 import traceback
 import numpy as np
+import json
 
 def get_rackets_url():
     # Get all rackets url from shopvnb.com
@@ -62,22 +63,61 @@ def get_racket_info(racket_url):
     html_text_racket = requests.get('https://shopvnb.com/'+str(racket_url)).text
     soup = BeautifulSoup(html_text_racket,'lxml')
     
-    image_url = soup.find('img', class_='img-responsive').get('src')
-    product_name = soup.find('h1', class_='title-product bk-product-name').text
-    brand = soup.find('a', class_='a-vendor').text.strip()
-    price = soup.find('span', class_='price product-price').text.split()[1] #TO DO to dollar
+    image_url = soup.find('img', class_=re.compile(r'\bimg-responsive\b')).get('src')
+    product_name = soup.find('h1', class_=re.compile(r'\btitle-product\b')).text
+    brand = soup.find('a', class_=re.compile(r'\ba-vendor\b')).text.strip()
+    price = soup.find('span', class_=re.compile(r'\bprice product-price\b')).text.split()[1]
     description = None #TO DO AI summaraize the description
-    state = "AVAILABLE"  if soup.find('span', class_='a-stock ') == 'Còn hàng' else "UNAVAILABLE"
+    state = "AVAILABLE"  if soup.find('span', class_=re.compile(r'\ba-stock\b')) == 'Còn hàng' else "UNAVAILABLE"
     stock = np.random.randint(24)+1 if soup.find('span', class_='a-stock').text == 'Còn hàng' else 0
     
     spec_table = soup.find('table', class_='table table-bordered')
     specs = {}
+    print("break_point_1")
     for spec in spec_table.find_all('tr')[:-1]:
         #TO DO translate to english
         spec_name = spec.b.text.strip()
         spec_value = spec.find_all('td')[-1].text.strip()
         specs[spec_name] = spec_value
         spec_value = spec.find_all('td')[-1].text
-    print(specs)
-url = 'vot-cau-long-vnb-v88-xanh-chinh-hang.html'
-get_racket_info(url)
+    #print(specs)
+    racket_info = {
+        "image_url": image_url,
+        "product_name": product_name,
+        "brand": brand,
+        "price": price, 
+        "description": description,
+        "status": state,
+        "sales": False,  # Assuming no sales information is available
+        "stock": stock,
+        "specs" : specs
+    }
+    return racket_info
+
+def process_single_page():
+    url_list = get_rackets_url()
+    rackets_list = []
+    for url in url_list:
+        try:
+           rackets_list.append(get_racket_info(url))
+        except Exception as e:
+            print(f"Error: {e}")
+            print(url)
+            continue
+    #find all unique key in list
+    unique_keys = set()
+    for racket in rackets_list:
+        unique_keys.update(racket["specs"].keys())
+    print(f"Unique keys in rackets_list: {unique_keys}")
+    # with open('rackets.json', 'w') as f:
+    #     json.dump(rackets_list, f, indent=4)
+    # print(f"Total rackets: {len(rackets_list)}")
+def debugger(url_list):
+    for url in url_list:
+        print(url)
+        print(get_racket_info(url))
+        print(f"complete{url}")
+if __name__ == "__main__":
+    bugged_urls = [ "vot-cau-long-vnb-carbon-training-150g.html", missing table]
+    debugger(bugged_urls)
+    #process_single_page()
