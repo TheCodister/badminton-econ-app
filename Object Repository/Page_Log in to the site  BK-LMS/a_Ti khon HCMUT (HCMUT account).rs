c6db2c01a9b889cf08af82d3b255d03ad661cabc
TheCodister <quangcuber002@gmail.com>
2025-05-23 15:07:24 +0700

    feat: add Test Login Suite

---
+++ Object Repository/Page_Log in to the site  BK-LMS/a_Ti khon HCMUT (HCMUT account).rs
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Ti khon HCMUT (HCMUT account)</name>
+   <tag></tag>
+   <elementGuidId>380503ba-c2a5-41e0-a702-edaa950a7645</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='region-main']/div/div/div/div/div[3]/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.btn.login-identityprovider-btn.btn-block</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Tài khoản HCMUT (HCMUT account)&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>ccebc86b-227b-4076-8ff7-e9116d294124</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn login-identityprovider-btn btn-block</value>
+      <webElementGuid>7e31a60f-da0e-4816-b88c-cf3ab514ac8c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://lms.hcmut.edu.vn/login/index.php?authCAS=CAS</value>
+      <webElementGuid>8d9fb7cd-c27a-459b-8a9f-6b923e3daa6b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        
+                    Tài khoản HCMUT (HCMUT account)
+                </value>
+      <webElementGuid>8870e1e1-5abb-44a7-b3d5-168eef05e3c2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;region-main&quot;)/div[@class=&quot;login-wrapper&quot;]/div[@class=&quot;login-container&quot;]/div[1]/div[@class=&quot;loginform&quot;]/div[@class=&quot;login-identityproviders&quot;]/a[@class=&quot;btn login-identityprovider-btn btn-block&quot;]</value>
+      <webElementGuid>ea95e783-deef-4dfd-bdf6-da6a2d6a2de9</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='region-main']/div/div/div/div/div[3]/a</value>
+      <webElementGuid>e2f7a7a2-fbec-4d09-8c7c-93affb4721c7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Log in using your account on:'])[1]/following::a[1]</value>
+      <webElementGuid>94d2da80-85ae-47ac-961d-5104b663913b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Log in to BK-LMS - HỆ THỐNG HỖ TRỢ GIẢNG DẠY VÀ HỌC TẬP'])[1]/following::a[1]</value>
+      <webElementGuid>ba9cb9bf-222c-431f-a290-ed197c8bbdeb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Admin'])[1]/preceding::a[1]</value>
+      <webElementGuid>469073ab-f59b-4d34-9651-229dcf13190f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Username'])[1]/preceding::a[2]</value>
+      <webElementGuid>c56fe766-ae24-48b0-b74a-0e4671250f16</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Tài khoản HCMUT (HCMUT account)']/parent::*</value>
+      <webElementGuid>414798bb-8e36-4f50-8a79-33cdedcb975b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://lms.hcmut.edu.vn/login/index.php?authCAS=CAS')]</value>
+      <webElementGuid>0b302693-a0b8-45f2-9024-08b60083c276</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/a</value>
+      <webElementGuid>587c6b38-5c64-44d8-8435-86b8dafc3f24</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://lms.hcmut.edu.vn/login/index.php?authCAS=CAS' and (text() = '
+                        
+                    Tài khoản HCMUT (HCMUT account)
+                ' or . = '
+                        
+                    Tài khoản HCMUT (HCMUT account)
+                ')]</value>
+      <webElementGuid>5fcf5842-d963-4697-9a43-2672296285a8</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Test Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a3168adc-69b8-4f70-a27a-34058495772f</testSuiteGuid>
+   <testCaseLink>
+      <guid>49157bb6-4e5e-4e01-8ce7-0b6660acf681</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test-login-lms</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
