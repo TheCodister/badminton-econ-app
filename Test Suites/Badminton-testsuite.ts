c3eebadd05e0b693f3a653b51efe1175cd3fd9f3
quangcuber002@gmail.com <katalon_g3_dummy_username>
2025-06-24 08:28:51 +0000

    Create "Badminton-testsuite" test suite

---
+++ Test Suites/Badminton-testsuite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+    <name>Badminton-testsuite</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>3c6ddddc-22d5-4a62-ad6e-cf25102b828e</testSuiteGuid>
+    <testCaseLink>
+        <guid>5c5e655a-c115-4ef1-8451-442e9f096dd6</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Test-login-lms</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>bb503dfd-d318-4210-a597-386d986c8046</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/New Test Case</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>be9ec7d6-64b0-430c-95d1-829d38793a1b</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Search feature for badminton ecommerce website</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>7d64d119-5f29-4acb-8522-421528c402ef</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Test-login-facebook</testCaseId>
+        <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    </testCaseLink>
+</TestSuiteEntity>
