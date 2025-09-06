183a4fac74151422d4b29ab96a3b110fe85995c0
TheCodister <quangcuber002@gmail.com>
2025-09-06 11:07:04 +0700

    feat: revert back to origin

--- constants/backend_url.ts
+++ constants/backend_url.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NEXT_PUBLIC_ENV === 'prod'
-    ? 'https://badminton-econ-app.onrender.com'
+    ? 'https://badminton-econ-app-q9zz.vercel.app'
     : 'http://localhost:3001'
